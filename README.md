a9eb3686b5a4702d98875154c2422d181b6a4d6e
sandro <rebub@vivaldi.net>
2021-09-25 14:32:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weather-app-react
+ A weather app made with the React framework.
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+ 
+const api = {
+  key: "45114f69bb4555d538da5b901b139bea",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+  return ( 
+    < div className = "App" >
+      Hello
+    </div>
+  );
+}
+
+export default App;
